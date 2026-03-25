36f6a66afc34a5f76000d47161452ea985db3e9b
HAIHUIHU-0504 <x24312797@student.ncirl.ie>
2026-03-25 00:05:43 +0000

    initial commits

---
+++ src/main/java/com/distsys/wire_fire_monitoring_system/Wire_Fire_Monitoring_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.distsys.wire_fire_monitoring_system;
+
+/**
+ *
+ * @author 123
+ */
+public class Wire_Fire_Monitoring_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
